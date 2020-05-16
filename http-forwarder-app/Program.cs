9d26b79a5e3f6df36aa6a3fbd907566ce4ff71b8
Ankit Mehta <ankit.mehta.developer@gmail.com>
2020-05-16 23:15:40 +0800

    Take fewer parameters from user, auto generate reqd parameters

--- http-forwarder-app/Program.cs
+++ http-forwarder-app/Program.cs
@@ -1,7 +1,9 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
+using System.Linq;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,37 +13,43 @@ namespace http_forwarder_app
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var initBuilder = new ConfigurationBuilder().AddCommandLine(args).AddEnvironmentVariables();
+            var initConfig = initBuilder.Build();
+            var sslPort = initConfig.GetValue<string>("SSL_PORT");
+            var newArgs = args.ToList();
+            {
+                if (!string.IsNullOrEmpty(sslPort))
+                {
+                    System.Console.WriteLine("Setting new env variables");
+                    var newAdditions = new Dictionary<string, string>
+                    {
+                        { "ASPNETCORE_HTTPS_PORT", sslPort },
+                        { "ASPNETCORE_URLS", "https://+;http://+" },
+                        { "ASPNETCORE_Kestrel__Certificates__Default__Password", Guid.NewGuid().ToString() },
+                        { "ASPNETCORE_Kestrel__Certificates__Default__Path", Path.Combine(Path.GetTempPath(), "aspnetapp.pfx") }
+                    };
+                    foreach(var pair in newAdditions)
+                    {
+                        Environment.SetEnvironmentVariable(pair.Key, pair.Value);
+                        newArgs.Add("--" + pair.Key);
+                        newArgs.Add(pair.Value);
+                    }
+                }
+            };
+            CreateHostBuilder(newArgs.ToArray()).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureLogging(l =>
                 {
                     l.AddConsole();
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    var port = webBuilder.GetSetting("ASPNETCORE_HTTPS_PORT");
-                    var certFile = webBuilder.GetSetting("CERT_PATH");
-                    var certKeyFile = webBuilder.GetSetting("CERT_KEY_PATH");
-                    webBuilder
-                        .ConfigureKestrel((so) =>
-                        {
-                            if (!string.IsNullOrEmpty(port) && !string.IsNullOrEmpty(certFile) && !string.IsNullOrEmpty(certKeyFile))
-                            {
-                                var portNum = int.Parse(port);
-                                so.ListenAnyIP(portNum, (lo) => lo.UseHttps(GetCertificate(certFile, certKeyFile)));
-                            }
-                        })
-                        .UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>();
                 });
-
-        private static X509Certificate2 GetCertificate(string certFile, string certKeyFile)
-        {
-            var rsa = RSA.Create();
-            rsa.ImportRSAPrivateKey(File.ReadAllBytes(certKeyFile), out var bytesRead);
-            return new X509Certificate2(certFile).CopyWithPrivateKey(rsa);
         }
     }
 }
